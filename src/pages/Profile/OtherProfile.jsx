235edf6d9feb1de9adfa5dd4a90ec34f33af667b
JongTKim <nh02111@naver.com>
2024-03-16 12:33:00 +0900

    친구버튼이 status에 따라 변화하도록 수정(친구신청, 친구신청 취소, 친구삭제, 친구수락 함수도 추가) 그에 따른 친구수도 변화하도록 수정

--- src/pages/Profile/OtherProfile.jsx
+++ src/pages/Profile/OtherProfile.jsx
@@ -3,18 +3,17 @@ import Layout from '../../components/Layout';
 import styles from './Profile.module.css';
 import PercentBar from "../../components/PercentBar/PercentBar";
 import { FaExchangeAlt } from "react-icons/fa";
-import {useLocation, useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { PiPlusCircleBold, PiPlusCircleFill } from "react-icons/pi";
 import LanguageList from './components/LanguageList';
-import Swal from "sweetalert2";
 
-export default function OtherProfile({myInformation, otherInformation}) {
-    const location = useLocation();
-    const [myInfo, setMyInfo] = useState(myInformation);
+export default function OtherProfile({otherInformation, handleOtherInfo}) {
     const [otherInfo, setOtherInfo] = useState(otherInformation);
     const navigate = useNavigate();
 
+    const [friendNum, setFriendNum] = useState(otherInfo?.friendnum) // 친구수락, 삭제시 친구수 관리를 위해
+    const [friendStatus, setFriendStatus] = useState(otherInfo.friendstatus); // 친구사이에 따라 버튼 변경
     const [maxCanLanguage, setMaxCanLanguage] = useState(); // 능숙도가 가장 높은 사용 언어
     const [maxWantLanguage, setMaxWantLanguage] = useState(); // 능숙도가 가장 높은 학습 언어
 
@@ -30,6 +29,12 @@ export default function OtherProfile({myInformation, otherInformation}) {
         return localStorage.getItem('accessToken'); // 쿠키 또는 로컬 스토리지에서 토큰을 가져옴
     };
 
+    /*
+    useEffect(() => {
+        setOtherInfo(otherInformation); // myInformation을 props로 받아서 업데이트
+    }, [otherInformation]);
+    */
+
     useEffect(() => {
         // 사용 언어 배열로 변환하여 업데이트한 후 능숙도가 높은 순으로 정렬
         const canLanguagesArray = Object.entries(otherInfo.canlanguages).map(([language, value]) => ({ language, value }));
@@ -39,17 +44,24 @@ export default function OtherProfile({myInformation, otherInformation}) {
         // 학습 언어 배열로 변환하여 업데이트한 후 능숙도가 높은 순으로 정렬
         const wantLanguagesArray = Object.entries(otherInfo.wantlanguages).map(([language, value]) => ({ language, value }));
         setWantLanguages(wantLanguagesArray);
-        setWantLanguages(prevWantLanguages => [...prevWantLanguages].sort((a, b) => b.value - a.value));  
+        setWantLanguages(prevWantLanguages => [...prevWantLanguages].sort((a, b) => b.value - a.value));
+
+        setFriendStatus(otherInfo.friendstatus);
+
     }, [otherInfo]);
 
     useEffect(() => {
         setMaxCanLanguage(canLanguages[0]);
     }, [canLanguages]);
-    
+
     useEffect(() => {
         setMaxWantLanguage(wantLanguages[0]);
     }, [wantLanguages]);
 
+    // friendStatus 상태값이 달라지면 Button 바뀜 (4개의 버튼)
+    useEffect(()=>{
+        friendButton();
+    },[friendStatus])
 
     // 언어 모달창 : 선택된 탭에 따라 해당 목록을 표시하는 함수
     const renderTabContent = () => {
@@ -73,14 +85,139 @@ export default function OtherProfile({myInformation, otherInformation}) {
         }
     };
 
-    const LoginWarning = () => {
-        Swal.fire({
-            icon: "warning",
-            title: "<div style='font-size: 21px; margin-bottom: 10px;'>로그인 후 이용해 주세요.</div>",
-            confirmButtonColor: "#8BC765",
-            confirmButtonText: "확인",
-        });
+    // 주의점 1번(친구삭제), 4번(친구수락)시 상대의 친구수가 변해야됨 ==> 리렌더링 필요
+    const friendButton = () => {
+        switch (friendStatus){
+            case 1 :
+                return (
+                    <button
+                        className={styles.buttonStyle}
+                        onClick={deleteFriend}>
+                        친구</button>
+                );
+
+            case 2 :
+                return (
+                    <button
+                        className={styles.buttonStyle}
+                        onClick={sendFriendRequest}>
+                        친구요청</button>
+                );
+
+            case 3 :
+                return (
+                    <button
+                        className={styles.buttonStyle}
+                        onClick={cancelRequest}>
+                        친구신청중..</button>
+                );
+            case 4 :
+                return (
+                    <button
+                        className={styles.buttonStyle}
+                        onClick={acceptReceivedRequest}>
+                        친구수락</button>
+                );
+        }
+    }
+
+    const deleteFriend = async () => {
+        try {
+            const token = getToken();
+
+            if(token) {
+                const response = await axios.delete('/api/auth/friend/deleteFriend', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    },
+                    data: {
+                        targetId: otherInfo.id
+                    }
+                });
+
+                if (response.status === 200) {
+                    alert("친구 삭제 성공");
+                    setFriendStatus(2);
+                    setFriendNum(friendNum-1);
+                } else if (response.status === 400) {
+                    console.log("클라이언트 오류");
+                } else if (response.status === 500) {
+                    console.log("서버 오류");
+                }
+            }
+            else {
+                alert("로그인 해주세요.");
+                navigate("/sign-in");
+            }
+        } catch (error) {
+            console.error('친구 삭제 중 에러 발생:', error);
+        }
     };
+
+    const acceptReceivedRequest = async () => {
+        try {
+            const token = getToken();
+
+            if(token) {
+                const response = await axios.post('/api/auth/friend/accept', {
+                    targetId: otherInfo.id
+                }, {
+                    headers: {
+                        Authorization: `Bearer ${token}` // 헤더에 토큰 추가
+                    }
+                });
+
+                if (response.status === 200) {
+                    alert("친구 수락 성공");
+                    setFriendStatus(1);
+                    setFriendNum(friendNum+1);
+                } else if (response.status === 400) {
+                    console.log("클라이언트 오류");
+                } else if (response.status === 500) {
+                    console.log("서버 오류");
+                }
+            }
+            else {
+                alert("로그인 해주세요.");
+                navigate("/sign-in");
+            }
+        } catch (error) {
+            console.error('친구 삭제 중 에러 발생:', error);
+        }
+    };
+
+    const cancelRequest = async () => {
+        try {
+            const token = getToken();
+
+            if(token) {
+                const response = await axios.delete('/api/auth/friend', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    },
+                    data: {
+                        targetId: otherInfo.id
+                    }
+                });
+
+                if (response.status === 200) {
+                    alert("친구 신청 취소 성공");
+                    setFriendStatus(2);
+                } else if (response.status === 400) {
+                    console.log("클라이언트 오류");
+                } else if (response.status === 500) {
+                    console.log("서버 오류");
+                }
+            }
+            else {
+                alert("로그인 해주세요.");
+                navigate("/sign-in");
+            }
+        } catch (error) {
+            console.error('친구 삭제 중 에러 발생:', error);
+        }
+    };
+
 
     //친구 신청
     const sendFriendRequest = async () => {
@@ -88,8 +225,8 @@ export default function OtherProfile({myInformation, otherInformation}) {
             const token = getToken(); // 토큰 가져오기
 
             if(token){ //로그인 O
-                const response = await axios.post('/api/auth/friend', { 
-                    targetId: otherInfo.id 
+                const response = await axios.post('/api/auth/friend', {
+                    targetId: otherInfo.id
                 }, {
                     headers: {
                         Authorization: `Bearer ${token}` // 헤더에 토큰 추가
@@ -97,6 +234,7 @@ export default function OtherProfile({myInformation, otherInformation}) {
                 });
                 if(response.status === 200){
                     alert("친구 신청 성공");
+                    setFriendStatus(3);
                 }
                 else if(response.status === 400){
                     console.log("친구 신청 클라이언트 에러");
@@ -106,8 +244,8 @@ export default function OtherProfile({myInformation, otherInformation}) {
                 }
             }
             else {
-                LoginWarning(); // 로그인 후 이용해 주세요.
-                navigate("/sign-in", {state: {from: location.pathname}}); // 현재 경로를 저장하고 로그인 페이지로 이동
+                alert("로그인 해주세요.");
+                navigate("/sign-in");
             }
         } catch (error) {
             console.error('친구 걸기 오류 발생:', error);
@@ -120,8 +258,8 @@ export default function OtherProfile({myInformation, otherInformation}) {
             const token = getToken(); // 토큰 가져오기
 
             if(token){ //로그인 O
-                const response = await axios.post('/api/auth/friend', { 
-                    targetId: otherInfo.id 
+                const response = await axios.post('/api/auth/friend', {
+                    targetId: otherInfo.id
                 }, {
                     headers: {
                         Authorization: `Bearer ${token}` // 헤더에 토큰 추가
@@ -133,19 +271,19 @@ export default function OtherProfile({myInformation, otherInformation}) {
                 else if(response.status === 400){
                     console.log("채팅 보내기 클라이언트 에러");
                 }
-                else if(response.status === 500){
+                else if(response.status ===  500){
                     console.log("채팅 보내기 서버 에러");
                 }
             }
             else {
-                LoginWarning(); // 로그인 후 이용해 주세요.
-                navigate("/sign-in", {state: {from: location.pathname}}); // 현재 경로를 저장하고 로그인 페이지로 이동
+                alert("로그인 해주세요.");
+                navigate("/sign-in");
             }
         } catch (error) {
             console.error('채팅 보내기 오류 발생:', error);
         }
     }
-    
+
 
     return (
         <Layout>
@@ -162,29 +300,19 @@ export default function OtherProfile({myInformation, otherInformation}) {
 
                     <div style={{display:"flex"}}>
                         {/* 친구 걸기/채팅 보내기 */}
-                    <button
-                        style={{
-                            borderRadius:"15px",
-                            backgroundColor:"#B7DAA1", 
-                            border:"0px",
-                            marginLeft: "10px",
-                            width:"200px",
-                            height:"30px"
-                        }}
-                        onClick={sendFriendRequest}
-                    >친구 걸기</button>
+                        {friendButton()}
 
-                    <button
-                        style={{
-                            borderRadius:"15px",
-                            backgroundColor:"#B7DAA1", 
-                            border:"0px",
-                            marginLeft: "10px",
-                            width:"200px",
-                            height:"30px"
-                        }}
-                        onClick={sendMessage}
-                    >채팅 보내기</button>
+                        <button
+                            style={{
+                                borderRadius:"15px",
+                                backgroundColor:"#B7DAA1",
+                                border:"0px",
+                                marginLeft: "10px",
+                                width:"200px",
+                                height:"30px"
+                            }}
+                            onClick={sendMessage}
+                        >채팅 보내기</button>
                     </div>
                 </div>
 
@@ -192,7 +320,7 @@ export default function OtherProfile({myInformation, otherInformation}) {
 
                     {/* 닉네임 */}
                     <div className={styles.name}>{otherInfo?.nickname}</div>
-                    
+
                     {/* 소개 */}
                     {otherInfo?.introduce && <div className={styles.intro}>{otherInfo.introduce}</div>}
 
@@ -201,7 +329,7 @@ export default function OtherProfile({myInformation, otherInformation}) {
                         <div className={styles.post}>게시글</div>
                         <div className={styles.postNum}>{otherInfo?.postnum}</div>
                         <div className={styles.friend}>친구</div>
-                        <div className={styles.friendNum}>{otherInfo?.friendnum}</div>
+                        <div className={styles.friendNum}>{friendNum}</div>
                     </div>
 
                     {/* 언어 */}
@@ -222,7 +350,7 @@ export default function OtherProfile({myInformation, otherInformation}) {
                     }
 
                     {/* 취미 */}
-                    {otherInfo?.hobbies && 
+                    {otherInfo?.hobbies &&
                         <div style={{ display: "flex", marginTop:"20px"}}>
                             {otherInfo.hobbies && otherInfo.hobbies.map((hobby, index) => (
                                 <div key={index} style={{ borderRadius:"15px", backgroundColor:"#C6CAC3", padding:"2px 15px", marginRight: "10px" }}>
@@ -237,22 +365,22 @@ export default function OtherProfile({myInformation, otherInformation}) {
                         <div className="modal fade show" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" style={{ display: "block", backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
                             <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                                 <div className="modal-content" style={{height:"450px"}}>
-                                        <ul className="nav nav-tabs">
-                                            <li className="nav-item">
-                                                <button 
-                                                    className={`nav-link ${activeTab === 'can' ? 'active' : ''}`} 
-                                                    style={{ width:"150px", backgroundColor: activeTab === 'can' ? '#B7DAA1' : 'white', color: "black"}}
-                                                    onClick={() => setActiveTab('can')}
-                                                >사용 언어</button>
-                                            </li>
-                                            <li className="nav-item">
-                                                <button 
-                                                    className={`nav-link ${activeTab === 'want' ? 'active' : ''}`} 
-                                                    style={{ width:"150px", backgroundColor: activeTab === 'want' ? '#B7DAA1' : 'white', color: "black"}}
-                                                    onClick={() => setActiveTab('want')}
-                                                >학습 언어</button>
-                                            </li>
-                                        </ul>
+                                    <ul className="nav nav-tabs">
+                                        <li className="nav-item">
+                                            <button
+                                                className={`nav-link ${activeTab === 'can' ? 'active' : ''}`}
+                                                style={{ width:"150px", backgroundColor: activeTab === 'can' ? '#B7DAA1' : 'white', color: "black"}}
+                                                onClick={() => setActiveTab('can')}
+                                            >사용 언어</button>
+                                        </li>
+                                        <li className="nav-item">
+                                            <button
+                                                className={`nav-link ${activeTab === 'want' ? 'active' : ''}`}
+                                                style={{ width:"150px", backgroundColor: activeTab === 'want' ? '#B7DAA1' : 'white', color: "black"}}
+                                                onClick={() => setActiveTab('want')}
+                                            >학습 언어</button>
+                                        </li>
+                                    </ul>
 
                                     <div className="modal-body">
                                         {renderTabContent()}
