abdfe5d46fcce1f4f49f6d3b037b4f5ee2b4922b
Noah-JUYong <103828756+Noah-JuYong@users.noreply.github.com>
2024-03-19 17:22:13 +0900

    스터디 수정

--- src/pages/Study/components/StudyCard.jsx
+++ src/pages/Study/components/StudyCard.jsx
@@ -4,10 +4,16 @@ import styles from './StudyCard.module.css'
 export const StudyCard = () => {
   return (
     <div className={styles.card}>
-      <div className={styles.title}></div>
-      <div className={styles.details}></div>
-      <div className={styles.rating}></div>
-
+      <div className={styles.top}>
+        <div className={styles.title}></div>
+      </div>
+      <div className={styles.mid}>
+        <div className={styles.method}></div>
+      </div>
+      <div className={styles.bot}>
+        <div className={styles.creator}></div>
+        <div className={styles.rating}></div>
+      </div>
     </div>
   )
 }
