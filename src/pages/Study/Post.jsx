0eb04d404ed7461ac7fa3023667d6fe6a7fc3a1a
Noah-JUYong <103828756+Noah-JuYong@users.noreply.github.com>
2024-03-26 13:48:04 +0900

    Update: react-quill 설치

--- src/pages/Study/Post.jsx
+++ src/pages/Study/Post.jsx
@@ -15,24 +15,21 @@ export const Post = () => {
   }
   return (
     <div className={styles.root}>
-      <form action="post" className={styles.form}>
-        <div></div>
-        <div>스터디 모집 예시를 참고해 작성해주세요. 꼼꼼히 작성하면 멋진 스터디 팀원을 만날 수 있을거예요.</div>
-        <div>
-          <input type="text" id='title' value={title} placeholder='제목에 핵심 내용을 요약해보세요.'/>
-        </div>
-        <div>
-          <input type="text" name="tags" id="tags" value={tags} />
-        </div>
-        <div>
-          <textarea name="content" id="content" cols="30" rows="10"></textarea>
-        </div>
-        <div>
-          <button type='button' onClick={() => navigate(-1)}>취소</button>
-          <button type='submit'>글 작성</button>
-        </div>
+      <div></div>
+      <div>스터디 모집 예시를 참고해 작성해주세요. 꼼꼼히 작성하면 멋진 스터디 팀원을 만날 수 있을거예요.</div>
+      <div>
+        <input type="text" id='title' value={title} placeholder='제목에 핵심 내용을 요약해보세요.'/>
+      </div>
+      <div>
+        <input type="text" name="tags" id="tags" value={tags} />
+      </div>
+      <div>
         
-      </form>
+      </div>
+      <div>
+        <button type='button' onClick={() => navigate(-1)}>취소</button>
+        <button type='submit'>글 작성</button>
+      </div>
     </div>
   )
 }
