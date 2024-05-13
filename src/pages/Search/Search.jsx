8977ebc5c49ba31597736d1f227098653c0ec8f8
junghada <pjunghada@naver.com>
2024-05-13 21:07:20 +0900

    통합검색 수정

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -53,7 +53,8 @@ const Search = () => {
             if (trimmedTag !== "" && !tags.includes(trimmedTag)) {
                 const newTags = [...tags, trimmedTag]; // 태그 배열에 현재 태그 추가
                 setTags(newTags); // 상태 업데이트
-                searchData(0, newTags); // 업데이트된 태그 배열을 searchData에 전달
+                // searchData(0, newTags); // 업데이트된 태그 배열을 searchData에 전달
+                getResultCount(newTags);
             }
             setTag(""); // 입력 필드 초기화
         } else if (e.key === 'Backspace' && tag === "") {
@@ -78,108 +79,6 @@ const Search = () => {
         const previousPath = location.state?.from || "/";
         navigate(previousPath);
     };
-
-    // 게시물 검색 요청
-    const searchData = async (page, newTags = tags) => {
-        setIsLoading(true); // 데이터 로딩 시작
-        let url = `/api/post/search?page=${page}&size=${pageSize}&category=NORMAL`;
-
-        // 검색어가 존재하는 경우
-        if (debouncedSearch) {
-            url += `&content=${debouncedSearch}`;
-        }
-
-        // 태그가 존재하는 경우
-        if (newTags.length > 0) {
-            const tagsQuery = newTags.map(tag => `tag=${(tag)}`).join('&');
-            console.log("tagsQuery:", tagsQuery);
-            url += `&${tagsQuery}`;
-        }
-
-        try {
-            const response = await axios.get(url);
-            if (response.status === 200) {
-                console.log("태그 배열 내용: ", newTags);
-                console.log("tags.length: ", newTags.length);
-                console.log("url: ", url);
-                console.log("받아온 게시물: ", response.data);
-                // setBoardList(prevBoardList => [...prevBoardList, ...response.data.content]); // 기존 목록에 추가
-                setBoardList(response.data.content);
-                setTotalElements(response.data.totalElements);
-                console.log("총 게시물 개수: ", totalElements);
-                setIsLoading(false); // 데이터 로딩 완료
-                setHasMore(((page+1) * pageSize) < response.data.totalElements);
-                console.log("api 요청하였습니다.");
-            }
-        } catch (error) {
-            if(error.response.status === 401) {
-                console.log("401 오류");
-                setIsLoading(false); // 에러 발생 시 로딩 상태 해제
-            }
-            else {
-                console.log("서버 오류 입니다.");
-                setIsLoading(false); // 에러 발생 시 로딩 상태 해제
-            }
-        }
-    };
-
-    // debounce를 위한 useEffect
-    useEffect(() => {
-        const handler = setTimeout(() => {
-            setDebouncedSearch(search);
-        }, 1000); // 사용자 입력이 멈춘 후 1초 뒤에 실행
-
-        return () => {
-            clearTimeout(handler); // timeout 취소
-        };
-    }, [search]);
-
-    // debouncedSearch가 변경될 때마다 검색 실행
-    useEffect(() => {
-        getResultCount();
-        if (debouncedSearch) { // debouncedSearch가 비어있지 않다면
-            setCurrentPage(0); // 페이지 처음부터 시작
-            if(searchType === 'post') {
-                setBoardList([]); // 검색 결과 목록을 초기화
-                searchData(0);
-            }
-            
-        } else {
-            // debouncedSearch가 비어있을 경우, 목록과 총 항목 수 초기화
-            if(searchType === 'post') setBoardList([]);
-        }
-    }, [debouncedSearch, tags]);
-
-    /*useEffect(() => {
-        console.log(currentPage);
-        console.log(isLoading);
-        console.log(hasMore);
-
-        if (currentPage > 0 && !isLoading && hasMore) {
-            searchData(currentPage);
-        }
-    }, [currentPage, isLoading, hasMore]);*/
-    useEffect(() => {
-        console.log(currentPage);
-        console.log(isLoading);
-        console.log(hasMore);
-
-        if (currentPage > 0 && hasMore) {
-            searchData(currentPage);
-        }
-    }, [currentPage, hasMore]);
-
-    // 마지막 요소를 관찰할 대상으로 설정
-    const lastBoardElementRef = useCallback(node => {
-        if (isLoading || !hasMore) return; // 로딩 중이거나 데이터가 더 없으면 관찰하지 않음
-        if (observer.current) observer.current.disconnect();
-        observer.current = new IntersectionObserver(entries => {
-            if (entries[0].isIntersecting) {
-                setCurrentPage(prevPageNumber => prevPageNumber + 1); // 현재 페이지 번호 업데이트
-            }
-        });
-        if (node) observer.current.observe(node); // 관찰 대상 설정
-    }, [isLoading, hasMore]);
 
     // 로그인 후 저장된 토큰 가져오는 함수
     const getToken = () => {
@@ -226,6 +125,189 @@ const Search = () => {
         }
     }; 
 
+    // 게시물 검색 요청
+    const searchPost = async (page, newTags = tags) => {
+        setIsLoading(true); // 데이터 로딩 시작
+        let url = `/api/post/search?page=${page}&size=${pageSize}&category=NORMAL`;
+
+        // 검색어가 존재하는 경우
+        if (debouncedSearch) {
+            url += `&content=${debouncedSearch}`;
+        }
+
+        // 태그가 존재하는 경우
+        if (newTags.length > 0) {
+            const tagsQuery = newTags.map(tag => `tag=${(tag)}`).join('&');
+            console.log("tagsQuery:", tagsQuery);
+            url += `&${tagsQuery}`;
+        }
+
+        try {
+            const response = await axios.get(url);
+            if (response.status === 200) {
+                console.log("태그 배열 내용: ", newTags);
+                console.log("tags.length: ", newTags.length);
+                console.log("url: ", url);
+                console.log("받아온 게시물: ", response.data);
+                setBoardList(preData => [...preData, ...response.data.content]); // 기존 목록에 추가
+                // setBoardList(response.data.content);
+                setIsLoading(false); // 데이터 로딩 완료
+                setHasMore(((page+1) * pageSize) < response.data.totalElements);
+                console.log("api 요청하였습니다.");
+            }
+        } catch (error) {
+            if(error.response.status === 401) {
+                console.log("401 오류");
+                setIsLoading(false); // 에러 발생 시 로딩 상태 해제
+            }
+            else {
+                console.log("서버 오류 입니다.");
+                setIsLoading(false); // 에러 발생 시 로딩 상태 해제
+            }
+        }
+    };
+
+
+    // 친구 검색 요청
+    const searchFriend = async (page) => {
+        setIsLoading(true); // 데이터 로딩 시작
+        const token = getToken();
+
+        let url = `/api/auth/friend/detail?page=${page}&size=${pageSize}`;
+
+        // 검색어가 존재하는 경우
+        if (debouncedSearch) {
+            url += `&nickname=${debouncedSearch}`;
+        }
+
+        try {
+            const response = await axios.get(url,  {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            if (response.status === 200) {
+                console.log("url: ", url);
+                setFriendList(preData => [...preData, ...response.data.content]);
+                console.log("받아온 찬구: ", response.data);
+                setIsLoading(false); // 데이터 로딩 완료
+                setHasMore(((page+1) * pageSize) < response.data.totalElements);
+            }
+        } catch (error) {
+            if(error.response.status === 401) {
+                console.log("401 오류");
+                setIsLoading(false); // 에러 발생 시 로딩 상태 해제
+                navigate("/sign-in");
+            }
+            else {
+                console.log("서버 오류 입니다.");
+                setIsLoading(false); // 에러 발생 시 로딩 상태 해제
+            }
+        }
+    };
+
+
+    // 멤버 검색 요청
+    const searchMember = async (page) => {
+        setIsLoading(true); // 데이터 로딩 시작
+        let url = `/api/member/search?page=${page}&size=${pageSize}`;
+
+        // 검색어가 존재하는 경우
+        if (debouncedSearch) {
+            url += `&nickname=${debouncedSearch}`;
+        }
+
+        try {
+            const response = await axios.get(url);
+            if (response.status === 200) {
+                console.log("url: ", url);
+                setMemberList(preData => [...preData, ...response.data.content]);
+                console.log("받아온 멤버: ", response.data);
+                setIsLoading(false); // 데이터 로딩 완료
+                setHasMore(((page+1) * pageSize) < response.data.totalElements);
+            }
+        } catch (error) {
+            if(error.response.status === 401) {
+                console.log("401 오류");
+                setIsLoading(false); // 에러 발생 시 로딩 상태 해제
+            }
+            else {
+                console.log("서버 오류 입니다.");
+                setIsLoading(false); // 에러 발생 시 로딩 상태 해제
+            }
+        }
+    };
+    
+    // debounce를 위한 useEffect
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedSearch(search);
+        }, 1000); // 사용자 입력이 멈춘 후 1초 뒤에 실행
+
+        return () => {
+            clearTimeout(handler); // timeout 취소
+        };
+    }, [search]);
+
+    // debouncedSearch가 변경될 때마다 검색 실행
+    useEffect(() => {
+        getResultCount();
+        if (debouncedSearch) { // debouncedSearch가 비어있지 않다면
+            setCurrentPage(0); // 페이지 처음부터 시작
+            if(searchType === 'post') {
+                setBoardList([]); // 검색 결과 목록을 초기화
+                searchPost(0);
+            }
+            else if(searchType === 'friend') {
+                setFriendList([]);
+                searchFriend(0);
+            }
+            else {
+                setMemberList([]);
+                searchMember(0);
+            }
+            
+        } else {
+            // debouncedSearch가 비어있을 경우, 목록과 총 항목 수 초기화
+            if(searchType === 'post') setBoardList([]);
+            else if(searchType === 'friend') setFriendList([]);
+            else setMemberList([]);
+        }
+    }, [debouncedSearch, tags, searchType]);
+
+    /*useEffect(() => {
+        console.log(currentPage);
+        console.log(isLoading);
+        console.log(hasMore);
+
+        if (currentPage > 0 && !isLoading && hasMore) {
+            searchData(currentPage);
+        }
+    }, [currentPage, isLoading, hasMore]);*/
+    useEffect(() => {
+        console.log(currentPage);
+        console.log(isLoading);
+        console.log(hasMore);
+
+        if (currentPage > 0 && hasMore) {
+            if(searchType==='post') searchPost(currentPage);
+            else if(searchType === 'friend') searchFriend(currentPage);
+            else searchMember(currentPage);
+        }
+    }, [currentPage, hasMore]);
+
+    // 마지막 요소를 관찰할 대상으로 설정
+    const lastElementRef = useCallback(node => {
+        if (isLoading || !hasMore) return; // 로딩 중이거나 데이터가 더 없으면 관찰하지 않음
+        if (observer.current) observer.current.disconnect();
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting) {
+                setCurrentPage(prevPageNumber => prevPageNumber + 1); // 현재 페이지 번호 업데이트
+            }
+        });
+        if (node) observer.current.observe(node); // 관찰 대상 설정
+    }, [isLoading, hasMore]);
+
     const renderContent = () => {
         switch (searchType) {
             case "post":
@@ -233,21 +315,8 @@ const Search = () => {
                     <div className="List-wrapper">
                         <div className="boardList-body">
                             {boardList && boardList.map((post, index) => {
-                                const key = `${post.postId}-${index}`; // 고유한 key 생성
-                                if (boardList.length === index + 1) { //현재 요소가 배열의 마지막 요소임을 의미
-                                    return <div ref={lastBoardElementRef} key={post.postId}> {/* 마지막 요소에 ref 할당 */}
-                                        <SearchCard
-                                            board_id={post.postId}
-                                            title={post.title}
-                                            content={post.content}
-                                            hashtag={post.tags}
-                                            username={post.writerName}
-                                            date={moment(post.createDate).add(9, "hours").format('YYYY년 MM월 DD일')}
-                                            likeCount={post.likeCount}
-                                        />
-                                    </div>
-                                } else {
-                                    return <SearchCard key={key}
+                                return <div ref={boardList.length === index + 1 ? lastElementRef : null} key={post.postId}> {/* 마지막 요소에 ref 할당 */}
+                                    <SearchCard
                                         board_id={post.postId}
                                         title={post.title}
                                         content={post.content}
@@ -256,7 +325,7 @@ const Search = () => {
                                         date={moment(post.createDate).add(9, "hours").format('YYYY년 MM월 DD일')}
                                         likeCount={post.likeCount}
                                     />
-                                }
+                                </div>
                             })}
                         </div>
                     </div>
@@ -265,9 +334,12 @@ const Search = () => {
                 return (    
                     <div className="List-wrapper">
                         <div className="friendList-body">
-                            {friendList.length > 0 && friendList.map((friend, index) => (
-                                <SearchUserCard key={index} user={friend} />
-                            ))}
+                            {friendList.length > 0 && friendList.map((friend, index) => {
+                                return <div ref={friendList.length === index + 1 ? lastElementRef : null} key={friend.id}>
+                                    <SearchUserCard user={friend} />
+                                </div>
+                                
+                            })}
                         </div>
                     </div>
                 );
@@ -275,12 +347,32 @@ const Search = () => {
                 return (
                     <div className="List-wrapper">
                         <div className="memberList-body">
-                            {memberList.length > 0 && memberList.map((user, index) => (
-                                <SearchUserCard key={index} user={user} />
-                            ))}
+                            {memberList.length > 0 && memberList.map((member, index) => {
+                                return <div ref={memberList.length === index + 1 ? lastElementRef : null} key={member.id}>
+                                    <SearchUserCard user={member} />
+                                </div>
+                            })}
                         </div>
                     </div>
                 );
+        }
+    }
+
+    const handleSearchButton = () => {
+        getResultCount();
+        setSearchType('post');
+        setCurrentPage(0);
+        if(searchType === 'post') {
+            setBoardList([]); // 검색 결과 목록을 초기화
+            searchPost(0);
+        }
+        else if(searchType === 'friend') {
+            setFriendList([]);
+            searchFriend(0);
+        }
+        else {
+            setMemberList([]);
+            searchMember(0);
         }
     }
     
@@ -294,7 +386,7 @@ const Search = () => {
                         <IoSearch className="input-icon" />
                         <input className="search-input" type="text" value={search} onChange={handleSearchChange} placeholder="검색어를 입력하세요"/>
                     </div>
-                    <button className="search-button" onClick={() => searchData(0)}>검색</button>
+                    <button className="search-button" onClick={handleSearchButton}>검색</button>
                 </div>
 
                 <div className="tag-container">
@@ -308,7 +400,7 @@ const Search = () => {
                                 </span>
                             ))}
                         </div>
-                        <input className="tag-input" type="text" value={tag} onChange={handleTagChange} onKeyDown={handleKeyDown} placeholder="태그로 검색해보세요!"/>
+                        <input className="tag-input" type="text" value={tag} onChange={handleTagChange} onKeyUp={handleKeyDown} placeholder="태그로 검색해보세요!"/>
                     </div>
                     <button className="reset-button" onClick={handleReset}><GrPowerReset className="reset-icon"/>초기화</button>
                 </div>
